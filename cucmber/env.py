1a211dd9367aaac296dd88dcdbda6e1e491093ea
Cahit <cahit10@hotmail.com>
2022-01-13 17:17:21 +0100

    fourth

--- cucmber/env.py
+++
@@ -1,5 +0,0 @@
-password = "cb061279"
-login_id = "cahit10@hotmail.com"
-
-e_logmail = "Enebak2027@ish.dk"
-e_pass = "Enes1110"
--- testaut/base/base_driver.py
+++
@@ -1,9 +0,0 @@
-
-
-class BaseDriver():
-
-    def __init__(self,driver,wait):
-        self.driver = driver
-        self.wait = wait
-
-    
